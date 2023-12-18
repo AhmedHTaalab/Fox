9eb8370f734b7451d1cbe5f792c4b9570f6e4b41
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-12-18 08:17:13 +0200

    implement write, delete, and update endpoints

--- src/main/java/com/SoA/Fox/EmployeeService.java
+++ src/main/java/com/SoA/Fox/EmployeeService.java
@@ -1,6 +1,6 @@
 package com.SoA.Fox;
 
-
+import com.SoA.Fox.Employee.KnownLanguage;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,6 @@ import java.util.List;
 
 import static com.SoA.Fox.Settings.XML_FILE_PATH;
 
-
 @Service
 public class EmployeeService {
 
@@ -22,7 +21,8 @@ public class EmployeeService {
 
         try {
             // Read JSON file and convert to List<Employee>
-            employees = objectMapper.readValue(new File(XML_FILE_PATH), new TypeReference<List<Employee>>() {});
+            employees = objectMapper.readValue(new File(XML_FILE_PATH), new TypeReference<List<Employee>>() {
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -30,7 +30,276 @@ public class EmployeeService {
         return employees != null ? employees : new ArrayList<>();
     }
 
+    public void deleteEmployeeByID(int targetID) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+
+        for (Employee empl : employees) {
+            if (empl.getEmployeeID() == targetID) {
+                employees.remove(empl);
+                break;
+            }
+        }
+
+        writeEmployeesToJsonFile(employees);
+    }
+
+    public void writeEmployeesToJsonFile(List<Employee> employees) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Write List<Employee> as JSON to file
+            objectMapper.writeValue(new File(XML_FILE_PATH), employees);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addEmployee(Employee employee) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        employees.add(employee);
+        writeEmployeesToJsonFile(employees);
+    }
+
+    public Employee updateEmployee(int ID, Employee employee) {
+        // Get all employees
+        List<Employee> employees = readEmployeesFromJsonFile();
+
+        Employee employeeToUpdate = null;
+
+        // Find employee to update
+        for (Employee empl : employees) {
+            // If employee found
+            if (empl.getEmployeeID() == ID) {
+                employeeToUpdate = empl;
+
+                // Remove old employee
+                employees.remove(empl);
+                break;
+            }
+        }
+
+        // If employee not found
+        if (employeeToUpdate == null) {
+            return null;
+        }
+
+        // Update employee
+        if (employee.getFirstName() != null)
+            employeeToUpdate.setFirstName(employee.getFirstName());
+
+        if (employee.getLastName() != null)
+            employeeToUpdate.setLastName(employee.getLastName());
+
+        if (employee.getDesignation() != null)
+            employeeToUpdate.setDesignation(employee.getDesignation());
+
+        if (employee.getKnownLanguages() != null)
+            employeeToUpdate.setKnownLanguages(employee.getKnownLanguages());
+
+        // Add updated employee
+        employees.add(employeeToUpdate);
+
+        // Write updated employees to file
+        writeEmployeesToJsonFile(employees);
+
+        return employeeToUpdate;
+    }
+
+    public List<Employee> getEmployeesByDesignation(String targetDesignation) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetDesignation = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            if (empl.getDesignation().equals(targetDesignation)) {
+                employeesWithTargetDesignation.add(empl);
+            }
+        }
+
+        return employeesWithTargetDesignation;
+    }
+
+    public List<Employee> getEmployeesByFirstName(String targetFirstName) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetFirstName = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            if (empl.getFirstName().equals(targetFirstName)) {
+                employeesWithTargetFirstName.add(empl);
+            }
+        }
+
+        return employeesWithTargetFirstName;
+    }
+
+    public List<Employee> getEmployeesByLastName(String targetLastName) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetLastName = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            if (empl.getLastName().equals(targetLastName)) {
+                employeesWithTargetLastName.add(empl);
+            }
+        }
+
+        return employeesWithTargetLastName;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguages(String targetKnownLanguage) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetKnownLanguage = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getLanguageName().equals(targetKnownLanguage)) {
+                    employeesWithTargetKnownLanguage.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetKnownLanguage;
+    }
+
+    public List<Employee> getEmployeesByEmployeeID(int targetEmployeeID) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetEmployeeID = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            if (empl.getEmployeeID() == targetEmployeeID) {
+                employeesWithTargetEmployeeID.add(empl);
+            }
+        }
+
+        return employeesWithTargetEmployeeID;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScore(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() == targetScore) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreGreaterThan(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() > targetScore) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreLessThan(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() < targetScore) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreBetween(int targetScore1, int targetScore2) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() >= targetScore1
+                        && knownLanguage.getScoreOutof100() <= targetScore2) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreBetweenExclusive(int targetScore1, int targetScore2) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() > targetScore1
+                        && knownLanguage.getScoreOutof100() < targetScore2) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreGreaterThanEqualTo(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() >= targetScore) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreLessThanEqualTo(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (knownLanguage.getScoreOutof100() <= targetScore) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
+
+    public List<Employee> getEmployeesByKnownLanguageScoreNotEqualTo(int targetScore) {
+        List<Employee> employees = readEmployeesFromJsonFile();
+        List<Employee> employeesWithTargetScore = new ArrayList<>();
+
+        for (Employee empl : employees) {
+            for (KnownLanguage knownLanguage : empl.getKnownLanguages()) {
+                if (!(knownLanguage.getScoreOutof100() == targetScore)) {
+                    employeesWithTargetScore.add(empl);
+                    break;
+                }
+            }
+        }
+
+        return employeesWithTargetScore;
+    }
 }
-
-
-
--- src/main/java/com/SoA/Fox/Settings.java
+++ src/main/java/com/SoA/Fox/Settings.java
@@ -1,8 +1,7 @@
 package com.SoA.Fox;
 
-
-
 public class Settings {
-    public static final String XML_FILE_PATH = "D:\\College\\SOA\\JavaSpring\\Assignment1\\test.json";
-//    public static final String XML_FILE_PATH = "/Users/Youssef/Projects/SoA-Assignment1/students.xml";
+    // public static final String XML_FILE_PATH =
+    // "D:\\College\\SOA\\JavaSpring\\Assignment1\\test.json";
+    public static final String XML_FILE_PATH = "/Users/Youssef/Projects/soa-assignment-3/Fox/test.json";
 }
